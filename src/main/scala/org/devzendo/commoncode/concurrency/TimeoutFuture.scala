a26e4a51db4ed81a53dc3b03305dc5d4f7d11396
Matt Gumbley <matt.gumbley@devzendo.org>
2018-01-22 08:35:19 +0000

    Implementation of TimeoutFuture - creates a Future that times out with Failure[TimeoutException] or returns a value.

--- src/main/scala/org/devzendo/commoncode/concurrency/TimeoutFuture.scala
+++ src/main/scala/org/devzendo/commoncode/concurrency/TimeoutFuture.scala
@@ -16,6 +16,82 @@
 
 package org.devzendo.commoncode.concurrency
 
-class TimeoutFuture {
+import java.util.concurrent.TimeoutException
+import java.util.concurrent.atomic.AtomicLong
 
+import org.devzendo.commoncode.timeout.TimeoutScheduler
+import org.slf4j.{Logger, LoggerFactory}
+
+import scala.concurrent.{ExecutionContext, Future, Promise}
+
+object TimeoutFuture {
+    val LOGGER: Logger = LoggerFactory.getLogger(TimeoutFuture.getClass)
+    val count = new AtomicLong(0)
+
+    /**
+      * Create a Future[T] that times out after the requested number of milliseconds, failing the Future with
+      * Failure[TimeoutException] in that case, then calling the onTimeoutBody to permit client code to tidy up.
+      * If execution of the executionBody completes before timeoutMs milliseconds have elapsed, its value is used to
+      * successfully complete the Future, and the timeout is cancelled, with the onTimeoutBody not called.
+      * @param timeoutMs the duration in milliseconds that the executionBody is given in which to execute.
+      * @param executionBody the body of code that returns a T, that will be used to successfully complete the Future
+      *                      if the timeout does not occur.
+      * @param onTimeoutBody an optional body of code that is executed if the executionBody takes longer than timeoutMs
+      *                      to complete.
+      * @param timeoutScheduler the implicit TimeoutScheduler used to schedule and cancel the timeout.
+      * @param executor the implicit ExecutionContext used to run the executionBody.
+      * @tparam T the type of the Future
+      * @return the Future[T] that can be used asynchronously.
+      */
+    def apply[T](timeoutMs: Long, executionBody: => T, onTimeoutBody: => Unit = {})(implicit timeoutScheduler: TimeoutScheduler, executor: ExecutionContext): Future[T] = {
+        val thisCount = count.getAndIncrement()
+        val messagePrefix = "TimeoutFuture #" + thisCount + ": "
+
+        val promise = Promise[T]()
+
+        val timeoutId = timeoutScheduler.schedule(timeoutMs, new Runnable {
+            override def run(): Unit = {
+                val str = messagePrefix + "Timed out after " + timeoutMs + "ms"
+                LOGGER.debug(str)
+
+                // Potential for race condition if executionBody has just completed with success: this setting of a
+                // failure will throw.
+                try {
+                    promise.failure(new TimeoutException(str))
+                } catch {
+                    case (ise: IllegalStateException) =>
+                        LOGGER.warn(messagePrefix + "Could not set Failure[TimeoutException] since Promise already completed") // otherwise, this can be ignored.
+                }
+
+                try {
+                    LOGGER.debug(messagePrefix + "Calling timeout handler")
+                    onTimeoutBody
+                    LOGGER.debug(messagePrefix + "Called timeout handler")
+                } catch {
+                    case (e: Exception) =>
+                        LOGGER.warn(messagePrefix + "Timeout handler threw " + e.getClass.getSimpleName + ": " + e.getMessage, e)
+                }
+            }
+        })
+
+        // Start executing the execution body....
+        LOGGER.debug(messagePrefix + "Scheduling execution with timeout id " + timeoutId)
+        Future({
+            // Potential for race condition if executionBody has just completed with timeout (failure): this setting of
+            // a success will throw.
+            try {
+                LOGGER.debug(messagePrefix + "Starting execution with timeout id " + timeoutId)
+                promise.success(executionBody)
+                LOGGER.debug(messagePrefix + "Finished successful execution with timeout id " + timeoutId)
+            } catch {
+                case (ise: IllegalStateException) =>
+                    LOGGER.warn(messagePrefix + "Could not set Success since Promise already completed with Failure[TimeoutException]") // otherwise, this can be ignored.
+            }
+            LOGGER.debug(messagePrefix + "Cancelling Timeout " + timeoutId)
+            timeoutScheduler.cancel(timeoutId)
+        })
+
+        promise.future
+    }
 }
+
--- src/test/scala/org/devzendo/commoncode/concurrency/TestTimeoutFuture.scala
+++ src/test/scala/org/devzendo/commoncode/concurrency/TestTimeoutFuture.scala
@@ -16,13 +16,23 @@
 
 package org.devzendo.commoncode.concurrency
 
+import java.util.concurrent.TimeoutException
+import java.util.concurrent.atomic.AtomicBoolean
+
 import org.devzendo.commoncode.logging.LoggingUnittest
-import org.junit.Test
+import org.devzendo.commoncode.timeout.{DefaultTimeoutScheduler, TimeoutId, TimeoutScheduler}
+import org.junit.{After, Assert, Before, Test}
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito.{times, verify, when}
 import org.scalatest.MustMatchers
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.junit.AssertionsForJUnit
 import org.scalatest.mock.MockitoSugar
 import org.slf4j.{Logger, LoggerFactory}
+
+import scala.concurrent.ExecutionContext.Implicits
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
 
 object TestTimeoutFuture {
     val LOGGER: Logger = LoggerFactory.getLogger(TestTimeoutFuture.getClass)
@@ -30,13 +40,110 @@ object TestTimeoutFuture {
 
 class TestTimeoutFuture extends AssertionsForJUnit with MustMatchers with ScalaFutures with MockitoSugar with LoggingUnittest {
 
-    import TestTimeoutFuture.LOGGER
-
     import scala.concurrent.ExecutionContext.Implicits.global
 
-    @Test
-    def hello(): Unit = {
+    implicit val timeoutScheduler: TimeoutScheduler = new DefaultTimeoutScheduler
 
+    @Before
+    def startScheduler(): Unit = {
+        timeoutScheduler.start()
+    }
+
+    @After
+    def stopScheduler(): Unit = {
+        timeoutScheduler.stop()
+    }
+
+    @Test(timeout = 4000L)
+    def timeoutFutureCompletingInTimeSucceeds(): Unit = {
+        val succeeded = new AtomicBoolean(false)
+
+        val future: Future[Boolean] = TimeoutFuture(1000L, {
+            val bool = true
+            succeeded.set(bool)
+            bool
+        })
+
+        ThreadUtils.waitNoInterruption(1500L) // wait for the timeout period to finish
+
+        Assert.assertTrue(succeeded.get())
+        future.value must be('defined)
+        future.value.get match {
+            case Success(x) =>
+                x mustBe true
+                // Can't test here that the timeout scheduler has cancelled the timeout
+            case Failure(_) =>
+                fail ("Should not have timed out")
+        }
+    }
+
+    @Test(timeout = 4000L)
+    def timeoutFutureCompletingCancelsTimeout(): Unit = {
+        val mockTimeoutScheduler = mock[TimeoutScheduler]
+        val timeoutId = new TimeoutId(69L)
+        when(mockTimeoutScheduler.schedule(ArgumentMatchers.eq(1000L), ArgumentMatchers.any[Runnable])).thenReturn(timeoutId)
+
+        TimeoutFuture(1000L, { true })(mockTimeoutScheduler, Implicits.global)
+
+        ThreadUtils.waitNoInterruption(1500L) // wait for the timeout period to finish
+
+        verify(mockTimeoutScheduler, times(1)).cancel(timeoutId)
+    }
+
+    @Test(timeout = 4000L)
+    def timeoutFutureCompletingAfterTimeoutTimeFails(): Unit = {
+        val succeeded = new AtomicBoolean(false)
+
+        val future: Future[Boolean] = TimeoutFuture(1000L, {
+            ThreadUtils.waitNoInterruption(2000L)
+            val bool = true
+            succeeded.set(bool)
+            bool
+        })
+
+        ThreadUtils.waitNoInterruption(500L)
+
+        // Initially, nothing happens...
+        Assert.assertFalse(succeeded.get())
+        future.value must not be 'defined
+
+        ThreadUtils.waitNoInterruption(1000L)
+
+        // should have finished with timeout by now
+        Assert.assertFalse(succeeded.get())
+        future.value must be('defined)
+        future.value.get match {
+            case Success(_) =>
+                fail ("Should not get a success from a timeout")
+            case Failure(x) =>
+                x mustBe a [TimeoutException]
+                x.getMessage must include("Timed out after 1000ms")
+                // It's implied that the timeout scheduler must have executed the timeout
+        }
+    }
+
+    @Test(timeout = 4000L)
+    def timeoutFutureCompletingAfterTimeoutTimeCallsOnTimeoutBody(): Unit = {
+        val succeeded = new AtomicBoolean(false)
+
+        val future: Future[Boolean] = TimeoutFuture(1000L, {
+            ThreadUtils.waitNoInterruption(2000L)
+            true
+        }, {
+            succeeded.set(true)
+        })
+
+        ThreadUtils.waitNoInterruption(1500L)
+
+        // should have finished with timeout by now
+        future.value must be('defined)
+        future.value.get match {
+            case Success(_) =>
+                fail ("Should not get a success from a timeout")
+            case Failure(_) =>
+                succeeded.get() must be(true)
+            // It's implied that the timeout scheduler must have executed the timeout
+        }
     }
 }
 
